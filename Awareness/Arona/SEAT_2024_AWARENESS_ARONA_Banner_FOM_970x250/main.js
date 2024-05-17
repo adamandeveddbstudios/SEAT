e0c1263c495fd1a1a063499db7684bc50cca6aa7
Mahesh Bhatt <141926871+darkCoderMahesh@users.noreply.github.com>
2024-05-17 17:06:44 +0530

    first commit

---
+++ Awareness/Arona/SEAT_2024_AWARENESS_ARONA_Banner_FOM_970x250/main.js
@@ -0,0 +1,36 @@
+var tl = new TimelineMax({ onComplete: endFrame, repeat: 0, repeatDelay: 3 });
+window.onload = function () {
+  initAd();
+};
+
+function endFrame() {
+  console.log("endFrame()");
+}
+
+
+function initAd() {
+
+  // Helper function
+  let domReady = (cb) => {
+    document.readyState === 'interactive' || document.readyState === 'complete'
+      ? cb()
+      : document.addEventListener('DOMContentLoaded', cb);
+  };
+
+  domReady(() => {
+    // Display body when DOM is loaded
+    document.body.style.visibility = 'visible';
+  });
+
+  //---------- START ANIMATION ------------
+  tl.addLabel("start")
+    .set("#cta", { opacity: 0, force3D: true })
+  // .set("#image1", { x: 0, y: 35, scale: 1.35 })
+
+  // // ----------Frame 1 ------------
+  tl.addLabel("frame1", "")
+    .to("#image1", 4.8, { x: 0, y: 0, scale: 1, ease: Linear.easeInOut, rotation: 0.1, force3D: true }, "frame1")
+
+  tl.addLabel("frame2", "4.8")
+    .to('#cta', 0.5, { opacity: 1, ease: Linear.easeInOut }, "frame2")
+}
